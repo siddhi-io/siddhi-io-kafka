b753332cb29251f9e021f37cc55518a064cf3268
sajith <sajithshn@gmail.com>
2017-09-07 23:25:51 +0530

    Adding Sequencing support to Source

--- component/src/main/java/org/wso2/extension/siddhi/io/kafka/source/KafkaConsumerThread.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/kafka/source/KafkaConsumerThread.java
@@ -24,6 +24,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.log4j.Logger;
+import org.wso2.extension.siddhi.io.kafka.sink.KafkaSink;
 import org.wso2.siddhi.core.stream.input.source.SourceEventListener;
 
 import java.util.ArrayList;
@@ -44,19 +45,27 @@ public class KafkaConsumerThread implements Runnable {
     private final KafkaConsumer<byte[], byte[]> consumer;
     // KafkaConsumer is not thread safe, hence we need a lock
     private final Lock consumerLock = new ReentrantLock();
+    private final String partitions[];
     private SourceEventListener sourceEventListener;
     private String topics[];
     private Map<String, Map<Integer, Long>> topicOffsetMap = new HashMap<>();
     private volatile boolean paused;
     private volatile boolean inactive;
     private List<TopicPartition> partitionsList = new ArrayList<>();
+    private Map<SequenceKey, Integer> lastReceivedSeqNoMap = null;
+    private String consumerThreadId;
+    private boolean isPartitionWiseThreading = false;
 
     KafkaConsumerThread(SourceEventListener sourceEventListener, String topics[], String partitions[],
-                        Properties props, Map<String, Map<Integer, Long>> topicOffsetMap) {
+                        Properties props, Map<String, Map<Integer, Long>> topicOffsetMap,
+                        boolean isPartitionWiseThreading) {
         this.consumer = new KafkaConsumer<>(props);
         this.sourceEventListener = sourceEventListener;
         this.topicOffsetMap = topicOffsetMap;
         this.topics = topics;
+        this.partitions = partitions;
+        this.isPartitionWiseThreading = isPartitionWiseThreading;
+        this.consumerThreadId = buildId();
         if (null != partitions) {
             for (String topic : topics) {
                 if (null == topicOffsetMap.get(topic)) {
@@ -135,15 +144,56 @@ public class KafkaConsumerThread implements Runnable {
                     consumerLock.unlock();
                 }
                 if (null != records) {
+
                     for (ConsumerRecord record : records) {
+                        int partition = record.partition();
                         String event = record.value().toString();
                         if (LOG.isDebugEnabled()) {
                             LOG.debug("Event received in Kafka Event Adaptor: " + event + ", offSet: " + record.offset()
-                                              + ", key: " + record.key() + ", topic: " + record.topic()
-                                              + ", partition: " + record.partition());
+                                    + ", key: " + record.key() + ", topic: " + record.topic()
+                                    + ", partition: " + partition);
                         }
                         topicOffsetMap.get(record.topic()).put(record.partition(), record.offset());
-                        sourceEventListener.onEvent(event, new String[0]);
+
+                        if (lastReceivedSeqNoMap == null) {
+                            sourceEventListener.onEvent(event, new String[0]);
+                        } else {
+                            int headerStartingIndex = event.indexOf(KafkaSink.SEQ_NO_HEADER_DELIMITER);
+                            if (headerStartingIndex > 0) {
+                                String header = event.substring(0, headerStartingIndex);
+                                String[] headerElements = header.split(KafkaSink.SEQ_NO_HEADER_FIELD_SEPERATOR);
+                                String sequenceId = headerElements[0];
+                                Integer seqNo = Integer.parseInt(headerElements[1]);
+                                SequenceKey sequenceKey = new SequenceKey(sequenceId, partition);
+                                Integer lastReceivedSeqNo = lastReceivedSeqNoMap.get(sequenceKey);
+
+                                if (lastReceivedSeqNo == null) {
+                                    lastReceivedSeqNo = -1;
+                                }
+
+                                if (lastReceivedSeqNo < seqNo) {
+                                    lastReceivedSeqNoMap.put(sequenceKey, seqNo);
+                                    String eventBody = event.substring(headerStartingIndex + 1);
+                                    sourceEventListener.onEvent(eventBody, new String[0]);
+                                    if (LOG.isDebugEnabled()) {
+                                    LOG.debug("Last Received SeqNo Updated to:" + seqNo + " for " +
+                                            "SeqKey:[" + sequenceKey.toString() + "] in Kafka consumer thread:"
+                                            + consumerThreadId);
+                                    }
+                                } else {
+                                    if (LOG.isDebugEnabled()) {
+                                    LOG.debug("Duplicate Message arrived at Kafka Consumer Thread:" +
+                                            consumerThreadId + ". SeqKey:[" + sequenceKey.toString() + "]"
+                                            + ", Latest SeqNo:" + lastReceivedSeqNo +
+                                            ", this message SeqNo:" + seqNo + ". Ignoring the message.");
+                                    }
+                                }
+                            } else {
+                                LOG.warn("'Sequenced' option is set to true in Kafka source configuration. " +
+                                        "But this message does not contain the sequence number in consumer thread :"
+                                        + consumerThreadId + ". Dropping the message");
+                            }
+                        }
                     }
                     try {
                         consumerLock.lock();
@@ -170,7 +220,88 @@ public class KafkaConsumerThread implements Runnable {
         inactive = true;
     }
 
+    public String buildId() {
+        StringBuilder key = new StringBuilder();
+        int count = topics.length - 1;
+        for (String topic : topics) {
+            key.append(topic);
+            if (--count >= 0) {
+                key.append(":");
+            }
+        }
+
+
+        if (partitions != null && isPartitionWiseThreading) {
+            count = partitions.length - 1;
+            key.append("-");
+            for (String partition : partitions) {
+                key.append(partition);
+                if (--count >= 0) {
+                    key.append(":");
+                }
+            }
+        }
+        return key.toString();
+    }
+
     Map<String, Map<Integer, Long>> getTopicOffsetMap() {
         return topicOffsetMap;
+    }
+
+    public String getConsumerThreadId() {
+        return consumerThreadId;
+    }
+
+    public Map<SequenceKey, Integer> getLastReceivedSeqNoMap() {
+        return lastReceivedSeqNoMap;
+    }
+
+    public void setLastReceivedSeqNoMap(Map<SequenceKey, Integer> seqNoMap) {
+        this.lastReceivedSeqNoMap = seqNoMap;
+    }
+}
+
+/**
+ * This class represents the key which message sequences are tracked against. Any two consequent Messages having the
+ * same sequence key must have increasing sequence numbers.
+ * SequenceKey consists of the SequenceId which is a unique identifier for each kafka Sink and the kafka partition Id.
+ */
+class SequenceKey {
+    private String sequenceId;
+    private int partitionId;
+
+    public SequenceKey(String sequenceId, int partitionId) {
+        this.sequenceId = sequenceId;
+        this.partitionId = partitionId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hash = 1;
+        hash = prime * hash + (sequenceId == null ? 0 : sequenceId.hashCode());
+        hash = prime * hash + partitionId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (!(object instanceof SequenceKey)) {
+            return false;
+        }
+
+        SequenceKey sequenceKey = (SequenceKey) object;
+
+        return ((sequenceKey.sequenceId.equals(this.sequenceId)) &&
+                (sequenceKey.partitionId == this.partitionId));
+    }
+
+    @Override
+    public String toString() {
+        return "SeqId:" + sequenceId + ", Partition" + ":" + partitionId;
     }
 }
