f9e0ff0a4ae80cbba7fa15ef7fcc4ecb15232a91
ramindu <rami.desilva@gmail.com>
2020-07-23 00:01:41 +0530

    Add test cases for trp properties

---
+++ component/src/main/java/io/siddhi/extension/io/kafka/Constants.java
@@ -0,0 +1,14 @@
+package io.siddhi.extension.io.kafka;
+
+/**
+ * Constants used in kafka executions.
+ * */
+public class Constants {
+    public static final String TRP_RECORD_TIMESTAMP = "record.timestamp";
+    public static final String TRP_EVENT_TIMESTAMP = "event.timestamp";
+    public static final String TRP_CHECK_SUM = "check.sum";
+    public static final String TRP_TOPIC = "topic";
+    public static final String TRP_PARTITION = "partition";
+    public static final String TRP_KEY = "key";
+    public static final String TRP_OFFSET = "offset";
+}
