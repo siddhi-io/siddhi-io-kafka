cc70389acc7d281a75cac770185af5082e571695
dilini-muthumala <dilini@wso2.com>
2020-12-02 22:27:22 +0530

    Initial commit

--- component/src/main/java/io/siddhi/extension/io/kafka/Constants.java
+++ component/src/main/java/io/siddhi/extension/io/kafka/Constants.java
@@ -11,4 +11,7 @@ public class Constants {
     public static final String TRP_PARTITION = "partition";
     public static final String TRP_KEY = "key";
     public static final String TRP_OFFSET = "offset";
+
+    /*prometheus reporte values*/
+    public static final String PROMETHEUS_REPORTER_NAME = "prometheus";
 }
